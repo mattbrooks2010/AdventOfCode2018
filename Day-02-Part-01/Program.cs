9e40c3e6abf663d2db16b693b484f64a4ab3c0b5
Matt Brooks <matt.brooks@enable.com>
2018-12-03 21:16:24 +0000

    Day 2

---
+++ Day-02-Part-01/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        // dotnet run < input.txt
+        static void Main(string[] args)
+        {
+            var pairCount = 0;
+            var tripletCount = 0;
+
+            string line = null;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                var info = GetBoxIdInfo(line);
+
+                if (info.HasPair)
+                {
+                    pairCount++;
+                }
+
+                if (info.HasTriplet)
+                {
+                    tripletCount++;
+                }
+            }
+
+            Console.WriteLine(pairCount);
+            Console.WriteLine(tripletCount);
+            Console.WriteLine(pairCount * tripletCount);
+        }
+
+        static BoxIdInfo GetBoxIdInfo(string line)
+        {
+            var letterCounts = new Dictionary<char, int>();
+
+            foreach (var letter in line)
+            {
+                if (letterCounts.ContainsKey(letter))
+                {
+                    letterCounts[letter] += 1;
+                }
+                else
+                {
+                    letterCounts[letter] = 1;
+                }
+            }
+
+            var info = new BoxIdInfo();
+
+            foreach (var entry in letterCounts)
+            {
+                if (entry.Value == 2)
+                {
+                    info.HasPair = true;
+                }
+
+                if (entry.Value == 3)
+                {
+                    info.HasTriplet = true;
+                }
+            }
+
+            return info;
+        }
+    }
+
+    struct BoxIdInfo
+    {
+        public bool HasPair { get; set; }
+        public bool HasTriplet { get; set; }
+    }
+}
---
+++ Day-02-Part-02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        // dotnet run < input.txt
+        static void Main(string[] args)
+        {
+            var ids = new List<string>();
+
+            string line = null;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                ids.Add(line);
+            }
+
+            for (var i = 0; i < ids.Count; i++)
+            {
+                for (var j = 0; j < ids.Count; j++)
+                {
+                    var diff = 0;
+
+                    for (var k = 0; k < ids[i].Length; k++)
+                    {
+                        if (ids[i][k] != ids[j][k])
+                        {
+                            diff++;
+                        }
+
+                        if (diff > 1)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (diff == 1)
+                    {
+                        for (var k = 0; k < ids[i].Length; k++)
+                        {
+                            if (ids[i][k] == ids[j][k])
+                            {
+                                Console.Write(ids[i][k]);
+                            }
+                        }
+
+                        Console.WriteLine();
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
