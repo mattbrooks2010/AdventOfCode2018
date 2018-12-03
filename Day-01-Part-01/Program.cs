53318968a95961b6a3c95a326a9c7ea97b912a09
Matt Brooks <matt.brooks@enable.com>
2018-12-03 20:35:38 +0000

    Day 1

---
+++ Day-01-Part-01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        // dotnet run < input.txt
+        static void Main(string[] args)
+        {
+            var frequency = 0;
+            string line = null;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                frequency += int.Parse(line);
+            }
+
+            Console.WriteLine(frequency);
+        }
+    }
+}
---
+++ Day-01-Part-02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        // dotnet run < input.txt
+        static void Main(string[] args)
+        {
+            var changes = new List<int>();
+            string line = null;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                changes.Add(int.Parse(line));
+            }
+
+            var frequency = 0;
+
+            var frequencies = new HashSet<int>();
+            frequencies.Add(frequency); // Add starting frequency
+
+            for (var i = 0; ; i++)
+            {
+                if (i >= changes.Count)
+                {
+                    // Reached the end of the input, so start from the beginning
+                    i = 0;
+                }
+
+                frequency += changes[i];
+
+                if (frequencies.Contains(frequency))
+                {
+                    // Reached first frequency twice, so stop
+                    break;
+                }
+
+                frequencies.Add(frequency);
+            }
+
+            Console.WriteLine(frequency);
+        }
+    }
+}
