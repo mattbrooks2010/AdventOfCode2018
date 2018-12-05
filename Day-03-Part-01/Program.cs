dfdc9097d5f6ccb8cf724c99ab445e1b42cd7e16
Matt Brooks <matt.brooks@enable.com>
2018-12-05 19:12:24 +0000

    Day 3

---
+++ Day-03-Part-01/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        static readonly Regex ClaimPattern = new Regex(@"^#\d+ @ (?<x>\d+),(?<y>\d+): (?<w>\d+)x(?<h>\d+)$");
+
+        // dotnet run < input.txt
+        static void Main(string[] args)
+        {
+            var claims = new List<Rectangle>();
+
+            string line = null;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                var claim = ParseClaim(line);
+                claims.Add(claim);
+            }
+
+            var overlapping = new HashSet<Point>();
+
+            // Compare each pair of claims exactly once
+            for (var i = 0; i < claims.Count; i++)
+            {
+                for (var j = i + 1; j < claims.Count; j++)
+                {
+                    // Calculate any intersection, i.e. any overlap
+                    var overlap = Rectangle.Intersect(claims[i], claims[j]);
+
+                    // Derive the all the X and Y coordinates of the overlap
+                    for (var k = 0; k < overlap.Width; k++)
+                    {
+                        var x = k + overlap.X;
+
+                        for (var l = 0; l < overlap.Height; l++)
+                        {
+                            var y = l + overlap.Y;
+
+                            // Track unique points (i.e. squares) only
+                            overlapping.Add(new Point(x, y));
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(overlapping.Count);
+        }
+
+        static Rectangle ParseClaim(string claim)
+        {
+            var match = ClaimPattern.Match(claim);
+
+            return new Rectangle(
+                int.Parse(match.Groups["x"].Value),
+                int.Parse(match.Groups["y"].Value),
+                int.Parse(match.Groups["w"].Value),
+                int.Parse(match.Groups["h"].Value));
+        }
+    }
+}
---
+++ Day-03-Part-02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        // dotnet run < input.txt
+        static void Main(string[] args)
+        {
+            var claims = new List<Claim>();
+
+            string line = null;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                var claim = Claim.Parse(line);
+                claims.Add(claim);
+            }
+
+            // Compare every claim to every other claim
+            for (var i = 0; i < claims.Count; i++)
+            {
+                // Assume this claim is the candidate
+                var candidate = true;
+
+                for (var j = 0; j < claims.Count; j++)
+                {
+                    // Don't compare a claim to itself
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    var overlap = Rectangle.Intersect(claims[i].Rectangle, claims[j].Rectangle);
+
+                    if (overlap.Width > 0 && overlap.Height > 0)
+                    {
+                        // Stop once we encounter an overlap
+                        candidate = false;
+                        break;
+                    }
+                }
+
+                if (candidate)
+                {
+                    // The current claim does not overlap with any other claim
+                    Console.WriteLine(claims[i].ID);
+
+                    return;
+                }
+            }
+        }
+    }
+
+    class Claim
+    {
+        static readonly Regex ClaimPattern = new Regex(@"^#(?<id>\d+) @ (?<x>\d+),(?<y>\d+): (?<w>\d+)x(?<h>\d+)$");
+
+        public Claim(string id, Rectangle rectangle)
+        {
+            ID = id;
+            Rectangle = rectangle;
+        }
+
+        public string ID { get; private set; }
+        public Rectangle Rectangle { get; private set; }
+
+        public static Claim Parse(string claim)
+        {
+            var match = ClaimPattern.Match(claim);
+
+            return new Claim(
+                match.Groups["id"].Value,
+                new Rectangle(
+                    int.Parse(match.Groups["x"].Value),
+                    int.Parse(match.Groups["y"].Value),
+                    int.Parse(match.Groups["w"].Value),
+                    int.Parse(match.Groups["h"].Value)));
+        }
+    }
+}
