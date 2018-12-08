c0ee09f4bd3872a4de90cf2188eebad4f4bb2aba
Matt Brooks <matt.brooks@enable.com>
2018-12-08 18:46:38 +0000

    Day 5 part 1

---
+++ Day-05-Part-01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        // cmd: dotnet run < input.txt
+        // ps:  gc input.txt | dotnet run
+        static void Main(string[] args)
+        {
+            var input = new List<char>(Console.ReadLine());
+
+            for (var i = 0; i < input.Count - 1; i++)
+            {
+                var a = (byte)input[i];
+                var b = (byte)input[i + 1];
+
+                if ((a ^ b) == 32)
+                {
+                    input.RemoveRange(i, 2);
+                    i -= 2;
+                }
+            }
+
+            Console.WriteLine(input.Count);
+        }
+    }
+}
